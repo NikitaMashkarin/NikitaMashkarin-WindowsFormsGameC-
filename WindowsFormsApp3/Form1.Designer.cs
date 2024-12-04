76bb5c1a2b4823d38c0e1795410641d2ab0d2084
NikitaMashkarin <mashkarinnikita@yandex.ru>
2024-12-04 22:56:50 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(240, 292);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(300, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(237, 199);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(303, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(237, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(303, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        string currentWord;
+        string hiddenWord;
+        string[] words = new string[]
+        {
+            "дом", "зима", "лес", "книга", "море", "зерно", "свет", "утюг", "птица", "рыба", "часы"
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            currentWord = getWord();
+            hiddenWord = closeWord(currentWord);
+            label1.Text = hiddenWord;
+            label2.Text = "Угадайте букву!";
+            textBox1.Enabled = true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 1)
+            {
+                char guessedLetter = textBox1.Text[0];
+                string updatedHiddenWord = openWord(currentWord, hiddenWord, guessedLetter);
+
+                if (hiddenWord != updatedHiddenWord)
+                {
+                    hiddenWord = updatedHiddenWord;
+                    label1.Text = hiddenWord;
+                    label2.Text = "Вы угадали букву!";
+                }
+                else
+                {
+                    label2.Text = "Вы не угадали букву.";
+                }
+
+                if (hiddenWord == currentWord)
+                {
+                    label2.Text = "Вы угадали слово!";
+                    textBox1.Enabled = false;
+                }
+
+                textBox1.Text = "";
+            }
+        }
+
+        public string getWord()
+        {
+            Random rnd = new Random(Guid.NewGuid().GetHashCode());
+            int rndInt = rnd.Next(0, words.Length);
+            return words[rndInt];
+        }
+
+        public string closeWord(string word)
+        {
+            return new string('*', word.Length);
+        }
+
+        public string openWord(string openWord, string closeWord, char letter)
+        {
+            char[] updatedWord = closeWord.ToCharArray();
+            bool letterFound = false;
+
+            for (int i = 0; i < openWord.Length; i++)
+            {
+                if (openWord[i] == letter)
+                {
+                    updatedWord[i] = letter; 
+                    letterFound = true;
+                }
+            }
+
+            return letterFound ? new string(updatedWord) : closeWord;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
